fb2f43e3c3fe0aa4c2f508e39c1b48a07bf15298
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2023-04-18 13:12:51 -0500

    ping wikipedia

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from scratchclient import ScratchSession
 import requests
+import json
 
 user = OS.getenv("USER")
 password = OS.getenv("PASSWORD")
@@ -8,4 +9,6 @@ connection = session.create_cloud_connection(837287681)
 
 @connection.on("set")
 def on_set(variable):
-  print(variable.name, variable.value, "aaaaaa")
+  parsed = "minecraft"
+  request = requests.get("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exlimit=max&explaintext&exintro&titles=" + parsed + "&redirects=").json()
+  
